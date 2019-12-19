c9952a63e9d25c7d04f37e7921778de7b8f31877
alm0st907 <garrett.rudisill@gmail.com>
2019-12-18 23:27:57 -0900

    init react app

---
+++ react-port/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Portfolio's are the bread and butter of software engineering, so here I am making one. It's also a good exercise in web development for someone who greatly prefers backend development. Enjoy this wild React ride.
+        </p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
