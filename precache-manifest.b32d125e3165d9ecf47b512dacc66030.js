a1689b89591e74260ca141365716d7d92daa4e16
alm0st907 <garrett.rudisill@gmail.com>
2019-12-23 17:53:14 -0900

    Updates

---
+++ precache-manifest.b32d125e3165d9ecf47b512dacc66030.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2c95a7d59bacbdd14a23a6371a140fd",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "47e6f0bb71555959045d",
+    "url": "/portfolio/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "677806186f72ed506575",
+    "url": "/portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "47e6f0bb71555959045d",
+    "url": "/portfolio/static/js/2.5c0a44ae.chunk.js"
+  },
+  {
+    "revision": "801eb6b718cfeb49e4699ab80a85e8cf",
+    "url": "/portfolio/static/js/2.5c0a44ae.chunk.js.LICENSE"
+  },
+  {
+    "revision": "677806186f72ed506575",
+    "url": "/portfolio/static/js/main.bd3a8878.chunk.js"
+  },
+  {
+    "revision": "7bb48d233b92dd25e6ea",
+    "url": "/portfolio/static/js/runtime-main.8632daae.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f9d963f69dda2ddb0eb244b1e06114ab",
+    "url": "/portfolio/static/media/rudisill_resume_2019_public.f9d963f6.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.862ff471abbcca63a83c24ccef18422c.js"
+  "/portfolio/precache-manifest.b32d125e3165d9ecf47b512dacc66030.js"
 );
 
 self.addEventListener('message', (event) => {
